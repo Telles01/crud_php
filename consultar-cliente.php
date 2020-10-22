dc6082698ae5411243560633c90269276184020c
Bernard Telles <bernardtelgon@gmail.com>
2020-10-21 23:47:46 -0300

    title change

--- consultar-cliente.php
+++ consultar-cliente.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Consultar</title>
+    <title>Consultar-Clientes</title>
 
     <link rel="stylesheet" href="materialize/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
